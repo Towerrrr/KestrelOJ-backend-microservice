489fc2c733c64c5ea9f14a4d5230eb0035d20f05
Towerrrr <1611993104@qq.com>
2025-04-19 16:15:45 +0800

    新增权限校验拦截器

---
+++ kestreloj-backend-service-client/src/main/java/com/t0r/kestrelojbackendserviceclient/interceptor/AuthInterceptor.java
@@ -0,0 +1,69 @@
+package com.t0r.kestrelojbackendserviceclient.interceptor;
+
+import com.t0r.kestrelojbackendcommon.annotation.AuthCheck;
+import com.t0r.kestrelojbackendcommon.common.ErrorCode;
+import com.t0r.kestrelojbackendcommon.exception.BusinessException;
+import com.t0r.kestrelojbackendmodel.model.entity.User;
+import com.t0r.kestrelojbackendmodel.model.enums.UserRoleEnum;
+import com.t0r.kestrelojbackendserviceclient.service.UserFeignClient;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 权限校验拦截器
+ */
+@Aspect
+@Component
+public class AuthInterceptor {
+
+    @Resource
+    private UserFeignClient userFeignClient;
+
+    /**
+     * 执行拦截
+     *
+     * @param joinPoint
+     * @param authCheck
+     * @return
+     */
+    @Around("@annotation(authCheck)")
+    public Object doInterceptor(ProceedingJoinPoint joinPoint, AuthCheck authCheck) throws Throwable {
+        String mustRole = authCheck.mustRole();
+        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+        // 当前登录用户
+        User loginUser = userFeignClient.getLoginUser(request);
+        UserRoleEnum mustRoleEnum = UserRoleEnum.getEnumByValue(mustRole);
+        // 不需要权限，放行
+        if (mustRoleEnum == null) {
+            return joinPoint.proceed();
+        }
+        // 获取当前用户的角色
+        UserRoleEnum userRoleEnum = UserRoleEnum.getEnumByValue(loginUser.getUserRole());
+        if (userRoleEnum == null) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 如果被封号，直接拒绝
+        if (UserRoleEnum.BAN.equals(userRoleEnum)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        // 如果需要管理员权限，判断当前用户是否有管理员权限
+        if (UserRoleEnum.ADMIN.equals(mustRoleEnum)) {
+            // 用户没有管理员权限，拒绝
+            if (!UserRoleEnum.ADMIN.equals(userRoleEnum)) {
+                throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+            }
+        }
+        // 通过权限校验，放行
+        return joinPoint.proceed();
+    }
+}
+
