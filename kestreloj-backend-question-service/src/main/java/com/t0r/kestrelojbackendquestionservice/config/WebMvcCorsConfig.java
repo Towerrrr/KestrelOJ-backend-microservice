571209b4babf49a0276599bf2e911eba28860dcd
Towerrrr <1611993104@qq.com>
2025-05-30 13:21:37 +0800

    题目服务跨域简单处理

---
+++ kestreloj-backend-question-service/src/main/java/com/t0r/kestrelojbackendquestionservice/config/WebMvcCorsConfig.java
@@ -0,0 +1,21 @@
+package com.t0r.kestrelojbackendquestionservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 处理跨域
+ */
+@Configuration
+public class WebMvcCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
