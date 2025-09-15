d980abfa2c6cc8db66f2cb0ebcb6364d444eb950
Towerrrr <1611993104@qq.com>
2025-09-15 22:00:47 +0800

    fix: 判题信息消息枚举

--- kestreloj-backend-model/src/main/java/com/t0r/kestrelojbackendmodel/model/enums/JudgeInfoMessageEnum.java
+++ kestreloj-backend-model/src/main/java/com/t0r/kestrelojbackendmodel/model/enums/JudgeInfoMessageEnum.java
@@ -11,18 +11,18 @@ import java.util.stream.Collectors;
  */
 public enum JudgeInfoMessageEnum {
 
-    ACCEPTED("成功", "Accepted"),
-    WRONG_ANSWER("答案错误", "Wrong Answer"),
-    COMPILE_ERROR("编译错误", "Compile Error"),
-    MEMORY_LIMIT_EXCEEDED("内存溢出", "Memory Limit Exceeded"),
-    TIME_EXCEEDED("超时", "Time Limit Exceeded"),
-    PRESENTATION_ERROR("展示错误", "Presentation Error"),
     WAITING("等待中", "Waiting"),
-    OUTPUT_LIMIT_EXCEEDED("输出溢出", "Output Limit Exceeded"),
+    ACCEPTED("成功", "Accepted"),
     DANGEROUS_OPERATION("危险操作", "Dangerous Operation"),
+    COMPILE_ERROR("编译错误", "Compile Error"),
     RUNTIME_ERROR("运行错误", "Runtime Error"),
+    TIME_EXCEEDED("超时", "Time Limit Exceeded"),
+    MEMORY_LIMIT_EXCEEDED("内存溢出", "Memory Limit Exceeded"),
+    WRONG_ANSWER("答案错误", "Wrong Answer"),
+    OUTPUT_LIMIT_EXCEEDED("输出溢出", "Output Limit Exceeded"),
+    PRESENTATION_ERROR("展示错误", "Presentation Error"),
     SYSTEM_ERROR("系统错误", "System Error");
-
+    
     private final String text;
 
     private final String value;
